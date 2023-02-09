01242f81a7fbccc268cf651f6dd1d63a36737619
Yannick <8698272+THEAccess@users.noreply.github.com>
2023-02-09 18:02:16 +0100

    Working prototype fetching data correctly into zipped 2d array

--- main.py
+++ main.py
@@ -5,10 +5,19 @@ import time
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import csv
 from selenium.webdriver.common.keys import Keys
 import requests
 
 target = "https://sssb.se/soka-bostad/sok-ledigt/lediga-bostader/?actionId=&omraden=Lappis&oboTyper=BOAS1&hyraMax="
+
+
+def nmap(func, array):
+    return list(map(func, array))
+
+
+def map_2d(func, array):
+    return list(nmap(lambda nested_array: nmap(func, nested_array), array))
 
 
 def get_website(name):
@@ -16,19 +25,23 @@ def get_website(name):
     driver.get(target)
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
-    headlines = [h3.find('a').text for h3 in  soup.find_all('h3', attrs={'class': 'ObjektTyp'})]
-    queueDays = soup.find_all('dd', attrs={ 'class': 'ObjektAntalInteresse'})
-    movingInDate = soup.find_all('dd', attrs={ 'class': 'ObjektInflytt'})
-    size = soup.find_all('dd', attrs={ 'class': 'ObjektYta'})
-    hyra = soup.find_all('dd', attrs={ 'class': 'ObjektHyra'})
-    floor = soup.find_all('dd', attrs={ 'class': 'Våning'})
-    print(queueDays)  # Press ⌘F8 to toggle the breakpoint.
-    print(movingInDate)  # Press ⌘F8 to toggle the breakpoint.
-    print(size)  # Press ⌘F8 to toggle the breakpoint.
+
+    headlines = nmap(lambda h3: h3.find('a'), soup.find_all('h3', attrs={'class': 'ObjektTyp'}))
+    raw_queue_days = soup.find_all('dd', attrs={'class': 'ObjektAntalIntresse'})
+    moving_in_date = soup.find_all('dd', attrs={'class': 'ObjektInflytt'})
+    size = soup.find_all('dd', attrs={'class': 'ObjektYta'})
+    rent = soup.find_all('dd', attrs={'class': 'ObjektHyra'})
+    floor = soup.find_all('dd', attrs={'class': 'ObjektVaning'})
+
+    t = [headlines, raw_queue_days, moving_in_date, size, rent, floor]
+    titles = ["Title", "Queue Days", "No. Applicants", "Moving in Date", "Flor", "Size", "Rent"]
+    r = map_2d(lambda e: e.text, t)
+
+    z = list(zip(*r))
+    z.insert(0, titles)
+    print(list(r))
+    print(z)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     get_website('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
