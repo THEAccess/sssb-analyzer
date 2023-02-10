211d808a8f16118d73443995364126347d0cf6f8
Yannick <8698272+THEAccess@users.noreply.github.com>
2023-02-10 16:39:24 +0100

    Improve code readability

--- main.py
+++ main.py
@@ -8,25 +8,26 @@ from utils import output, any_diff
 
 
 def run():
-    current_dir = get_current_working_dir()
-    prev_path = find_most_recent_file_path(current_dir)
-    content = get_website_content(base_url)
-    data = extract_sssb_data(content)
+    working_dir = get_current_working_dir()
+    # Get the path of the csv file for the most recent data fetched before this run (if it exists)
+    prev_file_path = find_most_recent_file_path(working_dir)
+    html_content = get_website_content(base_url)
+    data = extract_sssb_data(html_content)
 
-    if prev_path is not None:
-        previous = read_csv(prev_path)
+    output(data)
+
+    if prev_file_path is not None:
+        previous = read_csv(prev_file_path)
         if any_diff(previous, data):
-            print("{}: Found difference. Saving".format(datetime.datetime.now()))
-            save_to_csv(data, current_dir)
+            output("{}: Found difference. Saving".format(datetime.datetime.now()))
+            save_to_csv(data, working_dir)
         else:
-            print("Didn't find any difference. Not saving file")
+            output("Didn't find any difference. Not saving file")
     else:
-        save_to_csv(data, current_dir)
-
-    print(data)
+        save_to_csv(data, working_dir)
 
 
-def loop():
+def scraper_loop():
     while True:
         output("Running scheduled execution")
         run()
@@ -34,4 +35,4 @@ def loop():
 
 
 if __name__ == '__main__':
-    loop()
+    scraper_loop()
